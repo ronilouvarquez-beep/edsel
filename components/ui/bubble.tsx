813ae96b30d21e96f12b01eaafa1452f660eebb7
ronilouvarquez-beep <ronilou.varquez@nmsc.edu.ph>
2026-09-05 22:49:18 +0800

    fix the bubble error

--- components/ui/bubble.tsx
+++ components/ui/bubble.tsx
@@ -66,7 +66,7 @@ function BubbleContent({
   children,
   ...props
 }: React.ComponentProps<"div"> & {
-  render?: (props: React.HTMLAttributes<HTMLElement>) => React.ReactNode
+  render?: (props: React.ComponentProps<"div"> & { "data-slot": string }) => React.ReactNode
 }) {
   const contentClassName = cn(
     "w-fit max-w-full min-w-0 overflow-hidden rounded-xl border border-transparent px-3 py-2 text-sm leading-relaxed wrap-break-word whitespace-pre-wrap group-data-[align=end]/bubble:self-end [button]:text-left [button,a]:transition-colors [button,a]:outline-none [button,a]:focus-visible:border-ring [button,a]:focus-visible:ring-3 [button,a]:focus-visible:ring-ring/50",
